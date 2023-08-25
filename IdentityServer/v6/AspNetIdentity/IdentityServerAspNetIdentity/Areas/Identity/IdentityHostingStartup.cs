0b15705d4e4313f23056638360b57f8e2d61d4b1
David Klempfner <dklempfner@gmail.com>
2023-08-25 10:42:03 +1000

    Update IdentityHostingStartup.cs
    Removed unused using statements.

--- IdentityServer/v6/AspNetIdentity/IdentityServerAspNetIdentity/Areas/Identity/IdentityHostingStartup.cs
+++ IdentityServer/v6/AspNetIdentity/IdentityServerAspNetIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,4 @@
-﻿using System;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using IdentityServerAspNetIdentity.Data;
 
 [assembly: HostingStartup(typeof(IdentityServerAspNetIdentity.Areas.Identity.IdentityHostingStartup))]
 namespace IdentityServerAspNetIdentity.Areas.Identity
